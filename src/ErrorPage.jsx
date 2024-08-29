12f6400059c8ee2768eeb42281ad26227c3be9e7
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-08-29 12:46:00 -0700

    implement alternative router tutorial

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
 
-const ErrorPage = () => {
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
   return (
-    <div>
-      <h1>Oh no, this route doesn&apos;t exist!</h1>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
-      </Link>
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
     </div>
   );
-};
-
-export default ErrorPage;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,36 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import routes from "./routes";
+// import routes from "./routes";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css"
+import Root from "./routes/Root";
+import ErrorPage from "./ErrorPage";
+import Contact from "./routes/Contact";
+// const router = createBrowserRouter(routes)
 
-const router = createBrowserRouter(routes)
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <RouterProvider router={router} />
+//   </StrictMode>,
+// );
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root/>,
+    errorElement:<ErrorPage/>,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact/>,
+      }
+    ],
+  },
+])
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-);
+  </StrictMode>
+)
